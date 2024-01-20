ba7918fad172e2d6a7528a6a640a84071b30de36
abhishekbhonde <abhishekbhonde567@gmail.com>
2024-01-20 16:38:57 +0530

    socket connection established

---
+++ pages/api/socket.js
@@ -0,0 +1,18 @@
+import { Server } from "socket.io";
+
+const SocketHandler = (req,res) =>{
+    if(res.socket.server.io){
+        console.log("Socker already running");
+    }
+    else{
+        const io = new Server(res.socket.server)
+        res.socket.server.io = io
+
+        io.on('connection', (socket)=>{
+            console.log("Server is esatblished")
+        })
+    }
+res.send();
+}
+
+export default SocketHandler;
