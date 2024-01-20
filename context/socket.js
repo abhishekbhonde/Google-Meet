5c8c1ae809cb43af63ed5391f21718929b8919e0
abhishekbhonde <abhishekbhonde567@gmail.com>
2024-01-20 18:29:20 +0530

    set up peer js

--- context/socket.js
+++ context/socket.js
@@ -3,23 +3,23 @@ import { io } from "socket.io-client";
 import { useContext } from "react";
 const SocketContext = createContext(null);
 
-export const useSocket =() =>{
+export const useSocket = () => {
     const socket = useContext(SocketContext)
     return socket
 }
 
 
-export const SocketProvider =(props)=>{
-    const {children} = props;
+export const SocketProvider = (props) => {
+    const { children } = props;
     const [socket, setSocket] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         const connection = io();
         setSocket(connection)
         console.log('connection', connection)
-    },[])
+    }, [])
 
-    socket?.on('connect_error', async (err)=>{
+    socket?.on('connect_error', async (err) => {
         console.log("Error establishing socket", err)
         await fetch('/api/socket')
     })
--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,18 +1,18 @@
 import { Server } from "socket.io";
 
-const SocketHandler = (req,res) =>{
-    if(res.socket.server.io){
+const SocketHandler = (req, res) => {
+    if (res.socket.server.io) {
         console.log("Socker already running");
     }
-    else{
+    else {
         const io = new Server(res.socket.server)
         res.socket.server.io = io
 
-        io.on('connection', (socket)=>{
+        io.on('connection', (socket) => {
             console.log("Server is esatblished")
         })
     }
-res.send();
+    res.send();
 }
 
 export default SocketHandler;
